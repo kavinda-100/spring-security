cca4660d982e0ba3ba15f5081ee7e60d923babab
kavinda-100 <kavindarathnayake100@gmail.com>
2026-09-25 10:19:04 +0530

    Update validation, configurations, and endpoint in project setup: - Refine `RegisterRequest` validation rules for `name` field. - Adjust `application.yaml` with better formatting and added `show-sql` and `time_zone` properties. - Update security configuration to reflect new `/api/v1/auth/register` endpoint.

--- src/main/java/com/kavinda/spring_security/auth/dto/RegisterRequest.java
+++ src/main/java/com/kavinda/spring_security/auth/dto/RegisterRequest.java
@@ -1,13 +1,12 @@
 package com.kavinda.spring_security.auth.dto;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record RegisterRequest(
         @NotBlank(message = "Name is required")
-        @Min(value = 3, message = "Name must be at least 3 characters long")
+        @Size(min = 2, max = 50, message = "Name must be between 2 and 50 characters long")
         String name,
 
         @NotBlank(message = "Email is required")
